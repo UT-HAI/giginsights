6ba8111cb684751c0554589bda100aff65e9a856
Sarim Aleem <sarimaleem99@gmail.com>
2024-01-24 17:08:32 -0600

    did some formatting

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,43 +1,43 @@
 export default function Page() {
   return (
     <div className="CreateAccount w-80 h-96 relative bg-white">
-      <div className="Group76 w-72 h-9 left-[22px] top-[478px] absolute">
-        <div className="Rectangle3 w-72 h-9 left-0 top-0 absolute bg-stone-800 rounded" />
-        <div className="SignUp left-[113px] top-[10px] absolute text-white text-sm font-normal font-['Inter']">
+      <div className="Group76 w-72 h-9">
+        <div className="Rectangle3 w-72 h-9 bg-stone-800 rounded" />
+        <div className="SignUp text-white text-sm font-normal font-['Inter']">
           Sign up
         </div>
       </div>
-      <div className="Group10 h-16 left-[21px] top-[118px] absolute">
-        <div className="Name left-0 top-0 absolute text-stone-800 text-sm font-normal font-['Inter']">
+      <div className="Group10 h-16">
+        <div className="Name text-stone-800 text-sm font-normal font-['Inter']">
           Name
         </div>
-        <div className="Rectangle1 w-72 h-9 left-0 top-[31px] absolute rounded border border-stone-300" />
+        <div className="Rectangle1 w-72 h-9 rounded border border-stone-300" />
       </div>
-      <div className="Group1 h-16 left-[21px] top-[202px] absolute">
-        <div className="EmailId left-0 top-0 absolute text-stone-800 text-sm font-normal font-['Inter']">
+      <div className="Group1 h-16">
+        <div className="EmailId text-stone-800 text-sm font-normal font-['Inter']">
           Email ID
         </div>
-        <div className="Rectangle1 w-72 h-9 left-0 top-[31px] absolute rounded border border-stone-300" />
+        <div className="Rectangle1 w-72 h-9 rounded border border-stone-300" />
       </div>
-      <div className="Group11 w-72 h-16 left-[21px] top-[286px] absolute">
-        <div className="Group2 w-72 h-16 left-0 top-0 absolute">
-          <div className="Password left-[3px] top-0 absolute text-stone-800 text-sm font-normal font-['Inter']">
+      <div className="Group11 w-72 h-16">
+        <div className="Group2 w-72 h-16">
+          <div className="Password text-stone-800 text-sm font-normal font-['Inter']">
             Password
           </div>
-          <div className="Rectangle2 w-72 h-9 left-0 top-[31px] absolute rounded border border-zinc-300" />
+          <div className="Rectangle2 w-72 h-9 rounded border border-zinc-300" />
         </div>
-        <div className="PhEyeLight w-6 h-6 left-[236px] top-[37px] absolute" />
+        <div className="PhEyeLight w-6 h-6" />
       </div>
-      <div className="Group12 w-72 h-16 left-[21px] top-[370px] absolute">
-        <div className="Group9 w-72 h-16 left-0 top-0 absolute">
-          <div className="ReEnterPassword left-[3px] top-0 absolute text-stone-800 text-sm font-normal font-['Inter']">
+      <div className="Group12 w-72 h-16">
+        <div className="Group9 w-72 h-16">
+          <div className="ReEnterPassword text-stone-800 text-sm font-normal font-['Inter']">
             Re-enter password
           </div>
-          <div className="Rectangle2 w-72 h-9 left-0 top-[31px] absolute rounded border border-zinc-300" />
+          <div className="Rectangle2 w-72 h-9 rounded border border-zinc-300" />
         </div>
-        <div className="PhEyeLight w-6 h-6 left-[236px] top-[37px] absolute" />
+        <div className="PhEyeLight w-6 h-6" />
       </div>
-      <div className="NewAccount left-[21px] top-[64px] absolute text-stone-800 text-xl font-semibold font-['Inter']">
+      <div className="NewAccount text-stone-800 text-xl font-semibold font-['Inter']">
         New account
       </div>
     </div>
