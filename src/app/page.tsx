91975dca76dfe9fd6064952981dee77aac0e3f1e
Sarim Aleem <sarimaleem99@gmail.com>
2024-01-24 16:45:26 -0600

    created basic components for login and register page, need to fix some css layout stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,16 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="SplashScreen w-80 h-96 relative bg-white">
-      <div className="flex justify-center items-center w-72 h-9 left-[22px] top-[503px] absolute bg-white rounded border border-stone-800">
+    <div className="container mx-auto flex flex-col items-center justify-center min-h-screen">
+      <div className="flex">
+      <Image src="/home.svg" width={500} height={500} alt="home picture"></Image>
+      </div>
+      <div className="flex justify-center items-center w-72 h-9 bg-white rounded border border-stone-800 mb-4 mt-4">
         <Link href="/register" className="text-stone-800 text-sm font-normal font-['Inter']">Register</Link>
       </div>
-      <div className="flex justify-center items-center w-72 h-9 left-[21px] top-[453px] absolute bg-stone-800 rounded">
+      <div className="flex justify-center items-center w-72 h-9 bg-stone-800 rounded">
         <Link href="/login" className=" text-white text-sm font-normal font-['Inter']"> Login </Link>
       </div>
     </div>
